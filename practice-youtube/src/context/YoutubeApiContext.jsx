b37302fe092976185e11e991a228c8895b077e00
white-hun <121800818+white-hun@users.noreply.github.com>
2023-04-17 11:05:38 +0900

    Update YoutubeApiContext.jsx

--- practice-youtube/src/context/YoutubeApiContext.jsx
+++ practice-youtube/src/context/YoutubeApiContext.jsx
@@ -14,3 +14,9 @@ export function YoutubeApiProvider({ children }) {
 export function useYoutubeApi() {
   return useContext(YoutubeApiContext);
 }
+
+// new Youtube를 호출
+// 1. 새로운 객체 생성
+// 2. 넘겨받은 인수(client)와 함께 constructor가 자동으로 실행,
+//    이때 인수 client가 class<Youtube> - this.apiClient에 할당
+// 3. 이 과정을 거친 후 데이터를 가져오는 부분, youtube.search(keyword) 같은 객체 메서드를 호출할 수 있음
